2ca7d6788d17e54c223202e6fe623e555021da58
Marta Garcia Ferreiro <martagferreiro@fb.com>
2019-07-16 10:48:31 -0700

    explore page cardviews

---
+++ app/src/main/java/com/lingua/lingua/ExploreAdapter.java
@@ -0,0 +1,71 @@
+package com.lingua.lingua;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ExploreAdapter extends RecyclerView.Adapter<ExploreAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Object> users;
+
+    private TextView tvUser;
+    private TextView tvCaption;
+    private ImageView imageView;
+    private TextView timestamp;
+    private ImageView ivProfile;
+    private TextView tvLikeCount;
+    private Button heartButton;
+    private Button commentButton;
+
+    public ExploreAdapter(Context context, List<Object> users) {
+        this.context = context;
+//        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+//        final User user = users.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+ //       return users.size();
+        return 5;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.ivProfile);
+        }
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        users.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items
+    public void addAll(List<Object> list) {
+        users.addAll(list);
+        notifyDataSetChanged();
+    }
+}
