5efd5989e58faf37d1cb08dda22689b8df8f979f
Marta Garcia Ferreiro <martagferreiro@fb.com>
2019-07-17 12:16:00 -0700

    individual message layout for sent and received, defined theme colors, created user and message models

--- app/src/main/java/com/lingua/lingua/ChatDetailsActivity.java
+++ app/src/main/java/com/lingua/lingua/ChatDetailsActivity.java
@@ -3,12 +3,29 @@ package com.lingua.lingua;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lingua.lingua.models.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatDetailsActivity extends AppCompatActivity {
+
+    RecyclerView rvMessages;
+    private ChatDetailsAdapter adapter;
+    private List<Message> messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_details);
+        rvMessages = findViewById(R.id.fragment_chat_rv);
+        messages = new ArrayList<>();
+        adapter = new ChatDetailsAdapter(this, messages);
+        rvMessages.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvMessages.setLayoutManager(linearLayoutManager);
     }
 }
---
+++ app/src/main/java/com/lingua/lingua/ChatDetailsAdapter.java
@@ -0,0 +1,81 @@
+package com.lingua.lingua;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lingua.lingua.models.Message;
+
+import java.util.List;
+
+public class ChatDetailsAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+    private List<Message> messages;
+
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    TextView tvMessage, tvTimestamp;
+
+    public ChatDetailsAdapter(Context context, List<Message> messages) {
+        this.context = context;
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new ViewHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_received, parent, false);
+            return new ViewHolder(view);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = messages.get(position);
+        tvMessage.setText(message.getMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        // return messages.size();
+        return 5;
+    }
+
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messages.get(position);
+
+        if (message.getSender().getName().equals("Marta")) { // change to current user
+            // If the current user is the sender of the message
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            // If some other user sent the message
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvMessage = itemView.findViewById(R.id.message_tv);
+            tvTimestamp = itemView.findViewById(R.id.message_timestamp);
+        }
+    }
+}
