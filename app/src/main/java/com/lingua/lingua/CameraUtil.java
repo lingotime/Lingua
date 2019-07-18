71a40cebf97471c088860787e96ea9c916f73d6d
brianadouglas <briana.douglas@icloud.com>
2019-07-18 16:06:48 -0700

    The elements on the profile page mostly work except for the loading of the image, once taken, into the view. The dummy models have been deleted.

--- app/src/main/java/com/lingua/lingua/CameraUtil.java
+++ app/src/main/java/com/lingua/lingua/CameraUtil.java
@@ -42,49 +42,4 @@ public class CameraUtil {
         return file;
     }
 
-//    public String getRealPathFromURI(Context context, Uri contentUri) {
-//        Cursor cursor = null;
-//        try {
-//            String[] proj = {MediaStore.Images.Media.DATA};
-//            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//            cursor.moveToFirst();
-//            return cursor.getString(column_index);
-//        } finally {
-//            if (cursor != null) {
-//                cursor.close();
-//            }
-//        }}
-//
-//    private void launchCamera() {
-//        // create Intent to take the picture and return to the calling application
-//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        // create a file reference for future access
-//        photoFile = getPhotoFileUri(photoFileName);
-//
-//        // wrap the file object into a content provider
-//        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-//
-//        // checking that the result of the intent is not null and therefore that the app will not crash
-//        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-//            // start the image capture intent to take the photo
-//            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-//        }
-//    }
-//
-//    // Trigger gallery selection for a photo
-//    public void onPickPhoto(View view) {
-//        // Create intent for picking a photo from the gallery
-//        Intent intent = new Intent(Intent.ACTION_PICK,
-//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-//        // So as long as the result is not null, it's safe to use the intent.
-//        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-//            // Bring up gallery to select a photo
-//            startActivityForResult(intent, PICK_PHOTO_CODE);
-//        }
-//    }
-
-
 }
--- app/src/main/java/com/lingua/lingua/ProfileCreationActivity.java
+++ app/src/main/java/com/lingua/lingua/ProfileCreationActivity.java
@@ -1,23 +1,39 @@
 package com.lingua.lingua;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
 import com.hootsuite.nachos.NachoTextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Activity that allows the user to input their information relevant to the main functions of the app after getting past Auth
+ */
 
 public class ProfileCreationActivity extends AppCompatActivity {
 
 
-    private ImageView profilePicture;
+    private ImageView profilePicture; // will be taken from OAuth
     private TextView fullName;
     private TextView username;
     private NachoTextView originCountry;
@@ -26,6 +42,7 @@ public class ProfileCreationActivity extends AppCompatActivity {
     private NachoTextView targetCountries;
     private Button btnSubmit;
     private EditText bio;
+    public static final int CAMERA_ACTIVITY = 700; // return code for the profile picture activity
 
     private static final String[] COUNTRIES = new String[] {
             "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
@@ -92,6 +109,16 @@ public class ProfileCreationActivity extends AppCompatActivity {
         originCountry.setAdapter(adapterCountries);
 
 
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Launch the activity that will allow the user to take a picture
+                Intent intent = new Intent(ProfileCreationActivity.this, ProfilePicture.class);
+                startActivityForResult(intent, CAMERA_ACTIVITY);
+            }
+        });
+
+
 
         // TODO: Set the onlick listener for the Submit button and place the info into the User class connected to the database
 
@@ -106,5 +133,20 @@ public class ProfileCreationActivity extends AppCompatActivity {
 //                user.setBio(bio.getText().toString());
 //            }
 //        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == CAMERA_ACTIVITY) {
+            // extract the URI string, parse it into a URI object and set bitmap to the profile image view
+            String imagePath = data.getStringExtra("profilePicture");
+            Uri imageUri = Uri.parse(imagePath);
+            // load image into view
+            // TODO: fix the method to load in the image - probably from the stored URI
+            Glide.with(this)
+                    .load(new File(imageUri.getPath()))
+                    .into(profilePicture);
+        }
     }
 }
