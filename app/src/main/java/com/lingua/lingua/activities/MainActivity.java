45d0e7f14051274e2b158d620b32eeaa67e4e895
Fausto Zurita <faustodzurita@fb.com>
2019-07-22 14:35:59 -0700

    MainActivity has been finalized. Both frontend and backend.

--- app/src/main/java/com/lingua/lingua/activities/MainActivity.java
+++ app/src/main/java/com/lingua/lingua/activities/MainActivity.java
@@ -2,12 +2,12 @@ package com.lingua.lingua.activities;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lingua.lingua.R;
 import com.lingua.lingua.fragments.ChatFragment;
@@ -15,46 +15,46 @@ import com.lingua.lingua.fragments.ExploreFragment;
 import com.lingua.lingua.fragments.ConnectFragment;
 import com.lingua.lingua.fragments.ProfileFragment;
 
-/*
-Main Activity with bottom navigation bar that handles switching between fragments
-*/
+/* FINALIZED, DOCUMENTED, and TESTED: MainActivity displays a fragment, switchable with a bottom navigation. */
 
 public class MainActivity extends AppCompatActivity {
-
-    BottomNavigationView bottomNavigationView;
+    private FrameLayout fragmentFrame;
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation);
+        // associate views with java variables
+        fragmentFrame = findViewById(R.id.activity_main_fragment_frame);
+        bottomNavigation = findViewById(R.id.activity_main_bottom_navigation);
 
+        // manage fragments
         final FragmentManager fragmentManager = getSupportFragmentManager();
-        final Fragment profileFragment = new ProfileFragment();
-        final Fragment chatFragment = new ChatFragment();
         final Fragment exploreFragment = new ExploreFragment();
-        final Fragment notificationsFragment = new ConnectFragment();
+        final Fragment chatFragment = new ChatFragment();
+        final Fragment connectFragment = new ConnectFragment();
+        final Fragment profileFragment = new ProfileFragment();
 
         fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, exploreFragment).commit();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigation.setSelectedItemId(R.id.Explore);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.Explore:
-                        fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, exploreFragment).commit();
-                        return true;
                     case R.id.Chat:
                         fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, chatFragment).commit();
                         return true;
                     case R.id.Connect:
-                        fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, notificationsFragment).commit();
+                        fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, connectFragment).commit();
                         return true;
                     case R.id.Profile:
                         fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, profileFragment).commit();
                         return true;
                     default:
+                        fragmentManager.beginTransaction().replace(R.id.activity_main_fragment_frame, exploreFragment).commit();
                         return true;
                 }
             }
