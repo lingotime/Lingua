67af469718e14275819dc3334db7efad0669ca00
Fausto Zurita <faustodzurita@fb.com>
2019-07-24 21:56:28 -0700

    Refactored the basics for the TextChatActivity and the VideoChatActivity.

--- app/src/main/java/com/lingua/lingua/activities/TextChatActivity.java
+++ app/src/main/java/com/lingua/lingua/activities/TextChatActivity.java
@@ -1,80 +1,52 @@
 package com.lingua.lingua.activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.lingua.lingua.adapters.TextChatAdapter;
 import com.lingua.lingua.R;
-import com.lingua.lingua.models.User;
+import com.lingua.lingua.adapters.TextChatAdapter;
 import com.lingua.lingua.models.Message;
+import com.lingua.lingua.models.User;
 
 import java.util.ArrayList;
-import java.util.List;
 
-/*
-Activity for chatting with a specific friend, the recycler view contains all the messages the user
-and that friend have exchanged and user can send messages from here
-*/
+/* TextChatActivity allows a user to text another user. */
 
 public class TextChatActivity extends AppCompatActivity {
+    private User currentUser;
 
-    RecyclerView rvMessages;
-    private TextChatAdapter adapter;
-    private List<Message> messages;
-
-    private ImageView sendButtonIcon;
+    ArrayList<Message> messages;
+    TextChatAdapter messagesAdapter;
+    private TextView descriptionText;
+    private RecyclerView messagesTimeline;
+    private EditText messageField;
     private ImageButton sendButton;
-    private EditText etMessage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_chat);
-        rvMessages = findViewById(R.id.activity_text_chat_messages_timeline);
-        messages = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                Message message = new Message();
-                User sender = new User();
-                //sender.setFirstName("Marta");
-                //message.setMessage("Hey girl! How are you? I've been having a great day I hope you have too");
-                //message.setSender(sender);
-                messages.add(message);
-            } else {
-                Message message = new Message();
-                User sender = new User();
-                //sender.setFirstName("Cristina");
-                //message.setMessage("How are you? I'm doing well. Life is good, the family is doing well...");
-                //message.setSender(sender);
-                messages.add(message);
-            }
-        }
-
-        adapter = new TextChatAdapter(this, messages);
-        rvMessages.setAdapter(adapter);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setStackFromEnd(true);
-        rvMessages.setLayoutManager(linearLayoutManager);
-
+        // associate views with java variables
+        descriptionText = findViewById(R.id.activity_text_chat_description_text);
+        messagesTimeline = findViewById(R.id.activity_text_chat_messages_timeline);
+        messageField = findViewById(R.id.activity_text_chat_message_field);
         sendButton = findViewById(R.id.activity_text_chat_send_button);
-        etMessage = findViewById(R.id.activity_text_chat_message_field);
-        sendButtonIcon.setColorFilter(Color.argb(255, 255, 255, 255));
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO: send message
-            }
-        });
+        // load messages into timeline
+        // messages = loadMessages();
+        messages = new ArrayList<Message>();
+        messagesAdapter = new TextChatAdapter(this, messages);
+        messagesTimeline.setAdapter(messagesAdapter);
+
+        // display timeline
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        messagesTimeline.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/lingua/lingua/activities/VideoChatActivity.java
+++ app/src/main/java/com/lingua/lingua/activities/VideoChatActivity.java
@@ -1,16 +1,28 @@
 package com.lingua.lingua.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.lingua.lingua.R;
+import com.lingua.lingua.models.User;
+
+/* VideoChatActivity allows a user to videochat with another user. */
 
 public class VideoChatActivity extends AppCompatActivity {
+    private User currentUser;
+
+    private TextView descriptionText;
+    private FrameLayout videoFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_chat);
+
+        // associate views with java variables
+        descriptionText = findViewById(R.id.activity_video_chat_description_text);
+        videoFrame = findViewById(R.id.activity_video_chat_video_frame);
     }
 }
