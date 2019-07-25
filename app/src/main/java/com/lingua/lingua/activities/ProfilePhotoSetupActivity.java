8baef208248de24b12c39fe21919c5ded0f037ee
Fausto Zurita <faustodzurita@fb.com>
2019-07-24 17:47:10 -0700

    Added functionality to the profile photo setup activity, including cloud storage for photo files.

--- app/src/main/java/com/lingua/lingua/activities/ProfilePhotoSetupActivity.java
+++ app/src/main/java/com/lingua/lingua/activities/ProfilePhotoSetupActivity.java
@@ -1,8 +1,9 @@
 package com.lingua.lingua.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -17,175 +18,217 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.lingua.lingua.R;
-
-import java.io.ByteArrayOutputStream;
+import com.lingua.lingua.models.User;
+import org.parceler.Parcels;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Activity that allows the user to set their profile picture, whether it be from a picture that they take now, or a photo selected from the gallery.
- * This is called from the fragment where the user can edit their profile and from ProfileInfoSetupActivity where they enter the rest of their information.
- */
+/* FINALIZED, DOCUMENTED, and TESTED ProfilePhotoSetupActivity allows a user to setup their account profile photo. */
 
 public class ProfilePhotoSetupActivity extends AppCompatActivity {
+    private User currentUser;
 
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    private static final String TAG = "ProfilePictureActivity";
-    // member variables concerned with the camera activity
-    public final String APP_TAG = "MyCustomApp";
-    public String photoFileName = "photo.jpg";
-    File photoFile;
-    // PICK_PHOTO_CODE is a constant integer
-    public final static int PICK_PHOTO_CODE = 1046;
-
-    public Button btnCapture;
-    public Button btnChoose;
-    public ImageView ivImageTaken;
-    public Button btnProfile;
-    public Bitmap profilePicture = null;
+    File localProfilePhotoFile;
+    private Button takePhotoButton;
+    private Button selectPhotoButton;
+    private ImageView profilePreviewImage;
+    private Button setProfilePhotoButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_photo_setup);
 
-        btnCapture = findViewById(R.id.activity_profile_photo_setup_take_photo_button);
-        btnChoose = findViewById(R.id.activity_profile_photo_setup_select_photo_button);
-        ivImageTaken = findViewById(R.id.activity_profile_photo_setup_profile_image_preview);
-        btnProfile = findViewById(R.id.activity_profile_photo_setup_set_photo_button);
+        // associate views with java variables
+        takePhotoButton = findViewById(R.id.activity_profile_photo_setup_take_photo_button);
+        selectPhotoButton = findViewById(R.id.activity_profile_photo_setup_select_photo_button);
+        profilePreviewImage = findViewById(R.id.activity_profile_photo_setup_profile_image_preview);
+        setProfilePhotoButton = findViewById(R.id.activity_profile_photo_setup_set_photo_button);
 
-        // allows the user to change their profile picture
-        btnProfile.setOnClickListener(new View.OnClickListener() {
+        // unwrap the current user
+        currentUser = Parcels.unwrap(getIntent().getParcelableExtra("user"));
+
+        // launch camera view if the "take photo" button is clicked
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // set the selected image as the user's profile picture
-                if (photoFile == null || ivImageTaken.getDrawable() == null) {
-                    Log.e(TAG, "No photo to submit");
-                    return;
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                localProfilePhotoFile = getProfilePhotoFile("profile_photo.jpg");
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(ProfilePhotoSetupActivity.this, "com.lingua.fileprovider", localProfilePhotoFile));
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 1034);
                 }
-                // TODO: return the image URI to the previous activity, and add it to the current instance of the user
-                // generate the URI from the bitmap in order to pass the bitmap back to the ProfileCreation Activity
-
-                Intent data = new Intent();
-                data.putExtra("profilePicture", getImageUri(ProfilePhotoSetupActivity.this, profilePicture).toString());
-                setResult(700, data);
-                finish();
-
             }
         });
 
-        btnCapture.setOnClickListener(new View.OnClickListener() {
+        // load the current profile photo if one is available
+        Glide.with(this).load(currentUser.getUserProfilePhotoURL()).placeholder(R.drawable.man).apply(RequestOptions.circleCropTransform()).into(profilePreviewImage);
+
+        // launch photos view if the "select photo" button is clicked
+        selectPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                launchCamera();
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 1046);
+                }
             }
         });
 
-        btnChoose.setOnClickListener(new View.OnClickListener() {
+        // save new profile photo and return to previous page if the "set profile photo" button is clicked
+        setProfilePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                onPickPhoto(v);
+            public void onClick(View view) {
+                if (localProfilePhotoFile != null) {
+                    // get URI of the new profile photo
+                    Uri localProfilePhotoFileURI = Uri.fromFile(localProfilePhotoFile);
+
+                    // create references to cloud storage location
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    StorageReference storageReference = storage.getReference();
+                    StorageReference specificProfilesStorageReference = storageReference.child("profiles/" + currentUser.getUserID() + "_profile.jpg");
+
+                    // upload the new profile photo
+                    UploadTask uploadTask = specificProfilesStorageReference.putFile(localProfilePhotoFileURI);
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Log.d("ProfileSetupActivity", "The new profile photo was successfully uploaded.");
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e("ProfileSetupActivity", "The new profile photo failed to upload successfully.");
+                        }
+                    });
+
+                    // generate a URL link to the new profile photo's cloud storage location
+                    Task<Uri> generateURLTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (task.isSuccessful()) {
+                                return specificProfilesStorageReference.getDownloadUrl();
+                            } else {
+                                Log.e("ProfileSetupActivity", "There was an error generating the URL for the new profile photo.");
+                                return null;
+                            }
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                // get the URL link for the user's new profile photo
+                                Uri profilePhotoURI = task.getResult();
+
+                                // update the user's profile photo with the new profile photo URL link
+                                currentUser.setUserProfilePhotoURL(profilePhotoURI.toString());
+
+                                // save updates
+                                Firebase databaseReference = new Firebase("https://lingua-project.firebaseio.com/users");
+                                databaseReference.child(currentUser.getUserID()).setValue(currentUser);
+
+                                // return to info setup activity
+                                final Intent intent = new Intent(ProfilePhotoSetupActivity.this, ProfileInfoSetupActivity.class);
+                                intent.putExtra("user", Parcels.wrap(currentUser));
+                                startActivity(intent);
+                            } else {
+                                Log.e("ProfileSetupActivity", "There was an error generating the URL for the new profile photo.");
+                            }
+                        }
+                    });
+                } else {
+                    // return to info setup activity
+                    final Intent intent = new Intent(ProfilePhotoSetupActivity.this, ProfileInfoSetupActivity.class);
+                    intent.putExtra("user", Parcels.wrap(currentUser));
+                    startActivity(intent);
+                }
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    // Returns the File for a photo stored on disk given the fileName
-    public static File getPhotoFileUri(Context context, String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        if (requestCode == 1034) {
+            if (resultCode == RESULT_OK) {
+                Bitmap localProfilePhoto = BitmapFactory.decodeFile(localProfilePhotoFile.getAbsolutePath());
 
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
+                Glide.with(this).load(localProfilePhoto).placeholder(R.drawable.man).apply(RequestOptions.circleCropTransform()).into(profilePreviewImage);
+            } else {
+                Toast.makeText(this, "You did not take a photo.", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == 1046) {
+            if (data != null) {
+                Uri localProfilePhotoURI = data.getData();
+
+                localProfilePhotoFile = getProfilePhotoFile(this, localProfilePhotoURI);
+
+                try {
+                    Bitmap localProfilePhoto = MediaStore.Images.Media.getBitmap(this.getContentResolver(), localProfilePhotoURI);
+
+                    Glide.with(this).load(localProfilePhoto).placeholder(R.drawable.man).apply(RequestOptions.circleCropTransform()).into(profilePreviewImage);
+                } catch (IOException e) {
+                    Log.e("ProfileSetupActivity", "There was an error reading the selected photo file.");
+                }
+            }
+        }
+    }
+
+    private File getProfilePhotoFile(String fileName) {
+        // get the photo directory
+        File mediaStorageDirectory = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "Lingua");
+
+        // create the photo directory if it does not exist
+        if (!mediaStorageDirectory.exists() && !mediaStorageDirectory.mkdirs()) {
+            Log.e("ProfileSetupActivity", "There was an error creating a photo directory.");
         }
 
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        // get the profile photo file
+        File file = new File(mediaStorageDirectory.getPath() + File.separator + fileName);
 
         return file;
     }
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Context context, Uri contentUri) {
+    private File getProfilePhotoFile(Context context, Uri uri) {
+        // use a cursor to get profile photo selection
         Cursor cursor = null;
+
         try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            String[] projection = {MediaStore.Images.Media.DATA};
+
+            cursor = context.getContentResolver().query(uri, projection, null, null, null);
+
             cursor.moveToFirst();
-            return cursor.getString(column_index);
+
+            // get the profile photo file
+            File file = new File(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)));
+
+            return file;
+        } catch (Exception exception) {
+            Log.e("ProfileSetupActivity", "There was an error fetching the selected photo file.");
         } finally {
             if (cursor != null) {
                 cursor.close();
             }
-        }}
-
-    private void launchCamera() {
-        // create Intent to take the picture and return to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // create a file reference for future access
-        photoFile = getPhotoFileUri(this, photoFileName);
-
-        // wrap the file object into a content provider
-        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // checking that the result of the intent is not null and therefore that the app will not crash
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // start the image capture intent to take the photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
-    }
 
-    // Trigger gallery selection for a photo
-    public void onPickPhoto(View view) {
-        // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, PICK_PHOTO_CODE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                profilePicture = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // Load the taken image into a preview
-                ivImageTaken.setImageBitmap(profilePicture);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            if (requestCode == PICK_PHOTO_CODE) {
-                Uri photoUri = data.getData();
-                photoFile = new File(getRealPathFromURI(this, photoUri));
-                // Do something with the photo based on Uri
-                try {
-                    profilePicture = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // Load the selected image into a preview
-                ivImageTaken.setImageBitmap(profilePicture);
-            }
-        }
+        return null;
     }
 }
