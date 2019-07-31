cff48bae3732107e72b80ade30ae2ef480ea7f05
brianadouglas <briana.douglas@icloud.com>
2019-07-31 13:47:34 -0700

    Fixes to the chat fragment

--- app/src/main/java/com/lingua/lingua/fragments/ChatFragment.java
+++ app/src/main/java/com/lingua/lingua/fragments/ChatFragment.java
@@ -108,6 +108,7 @@ public class ChatFragment extends Fragment {
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
 
+
         queryChats();
     }
 
